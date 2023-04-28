3451f6d268019ab9fb51cb5b11d89f67ddb26798
TASRA SABAH <Sabahtasra123@gmail.com>
2023-04-27 19:20:04 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letters
+ * @c: char to check the result
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int cr)
+{
+	if (cr >= 'A' && cr <= 'Z')
+	return (1);
+	else
+	return (0);
+}
+
