fc3beb7ed41fd92ec5563315fa1c89a85284c0f4
TASRA SABAH <Sabahtasra123@gmail.com>
2023-04-27 18:11:57 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,27 +10,27 @@
  * Return: void
  */
 
-void print_remaining_days(int mn, int d, int yr)
+void print_remaining_days(int mounth, int day, int year)
 {
-if ((yr % 100 == 0 && yr % 400 == 0) || (yr % 4 == 0))
+if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 {
-if (mn > 2 && d >= 60)
+if (mounth > 2 && day >= 60)
 {
-d++;
+day++;
 }
-printf("Day of the year: %d\n", d);
-printf("Remaining days: %d\n", 366 - d);
-}
-else
-{
-if (mn == 2 && d == 60)
-{
-printf("Invalid date: %02d/%02d/%04d\n", mn, d - 31, yr);
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-printf("Day of the year: %d\n", d);
-printf("Remaining days: %d\n", 365 - d);
+if (mounth == 2 && day == 60)
+{
+printf("Invalid date: %02d/%02d/%04d\n", mounth, day - 31, year);
+}
+else
+{
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,6 +6,6 @@
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
-void print_remaining_days(int mn, int d, int yr);
+void print_remaining_days(int mounth, int day, int year);
 
 #endif /* MAIN_H */
