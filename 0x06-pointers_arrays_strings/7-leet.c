6b7aea9aff8b574fe3ae5b888180a611ee770ddc
TASRA SABAH <Sabahtasra123@gmail.com>
2023-05-05 17:35:55 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
+ */
+char *leet(char *n)
+{
+	int i, j;
+	char a1[] = "aAeEoOtTlL";
+	char a2[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == a1[j])
+			{
+				n[i] = a2[j];
+			}
+		}
+	}
+	return (n);
+}
+
