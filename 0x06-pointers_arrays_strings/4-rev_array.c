4ed0d500417798e5e043e70415981a6e07793acc
TASRA SABAH <Sabahtasra123@gmail.com>
2023-05-05 17:18:36 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of elements of array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int tmp;
+
+	for (i = 0; i < n--; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n];
+		a[n] = tmp;
+	}
+}
+
