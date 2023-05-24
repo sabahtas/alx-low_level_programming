c41f5fcfdc332373f1095ba6e4b76d80af7eab1f
TASRA SABAH <Sabahtasra123@gmail.com>
2023-05-24 09:34:20 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * is_98 - check if a number is equal to 98
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int is_98(int elem)
+{
+    return (98 == elem);
+}
+
